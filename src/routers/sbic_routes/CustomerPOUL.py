b8b0fceb233a4591c2f470163c6cf124ca4144a8
erar404 <it.arellanoerwin@gmail.com>
2026-04-23 15:20:32 +0800

    added api authentication

--- src/routers/sbic_routes/CustomerPOUL.py
+++ src/routers/sbic_routes/CustomerPOUL.py
@@ -3,6 +3,7 @@ import logging
 from google.cloud import logging as cloud_logging
 from fastapi import HTTPException, Query, status, Depends, APIRouter
 from src.routers.bigquery_bridge import BigqueryBridge
+from src.config import pass_key
 
 # Instantiate a Cloud Logging client
 client = cloud_logging.Client()
@@ -12,9 +13,11 @@ logger = logging.getLogger('customerpoul')
 
 customerpoul_router = APIRouter(prefix="/customerpoul", tags=["CustomerPOUL"])
 
-@customerpoul_router.post("/runbridge/method/")
-async def run_customerpoul_bridge(method: str = 'manual'):
+@customerpoul_router.post("/runbridge/")
+async def run_customerpoul_bridge(method: str = 'manual', passkey: str = Query(..., description="Passkey for authentication")):
     try:
+        if passkey != pass_key:  # Replace with actual passkey validation logic
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid passkey")
         bridge = BigqueryBridge(logger, method, group_code='customerpoul')
         result = bridge.main()
         return result
@@ -23,9 +26,12 @@ async def run_customerpoul_bridge(method: str = 'manual'):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 
-@customerpoul_router.post("/runbridge/onlinesalespo/method/")
-async def run_online_sales_po_bridge(method: str = 'manual'):
+@customerpoul_router.post("/runbridge/onlinesalespo/")
+async def run_online_sales_po_bridge(method: str = 'manual', passkey: str = Query(..., description="Passkey for authentication")):
     try:
+        if passkey != pass_key:  # Replace with actual passkey validation logic
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid passkey")
+        
         bridge = BigqueryBridge(logger, method, group_code='onlinesalespo')
         result = bridge.main()
         return result
--- src/routers/sbic_routes/CustomerRA.py
+++ src/routers/sbic_routes/CustomerRA.py
@@ -4,7 +4,7 @@ import logging
 from google.cloud import logging as cloud_logging
 from fastapi import HTTPException, Query, status, Depends, APIRouter
 from src.routers.bigquery_bridge import BigqueryBridge
-
+from src.config import pass_key
 
 # Instantiate a Cloud Logging client
 client = cloud_logging.Client()
@@ -14,9 +14,11 @@ logger = logging.getLogger('customer_ra')
 
 customer_ra_router = APIRouter(prefix="/customerra", tags=["CustomerRA"])
 
-@customer_ra_router.post("/runbridge/method/")
-async def run_remittance_advice_bridge(method: str = 'manual'):
+@customer_ra_router.post("/runbridge/")
+async def run_remittance_advice_bridge(method: str = 'manual', passkey: str = Query(..., description="Passkey for authentication")):
     try:
+        if passkey != pass_key:  # Replace with actual passkey validation logic
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid passkey")
         bridge = BigqueryBridge(logger, method, group_code='customerra')
         result = bridge.main()
         return result
